100450a0e004628d433ed3db05c4c5e5632560db
Ricardo de Souza Bittar <ricardo.bittar@dinfor.uniceub>
2023-08-28 10:06:21 -0300

    exerciciosresolvidos

--- exercicio9.go
+++ exercicio9.go
@@ -1,9 +1,20 @@
-package algoritmos_iniciais_ricardosbittar
+package main
+
+import "fmt"
 
 func main() {
-	var t int
-	var juros float32
-	valorInicial := 5000
-	valorFinal := valorInicial*t ^ juros
+	var t float64
+	var valorInicial float64
+
+	fmt.Println("Qual o valor inicial? ")
+	fmt.Scanln(&valorInicial)
+	fmt.Println("Quantos meses? ")
+	fmt.Scanln(&t)
+
+	juros := 2.3
+
+	jurostempo := t ^ juros
+	valorFinal := valorInicial * jurostempo
+	fmt.Println(valorFinal)
 
 }
